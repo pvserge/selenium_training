c84d33fc3c15998cb713a96c1f917fa19feda4dd
sergepv <serge@ncryptedcloud.com>
2019-07-03 15:14:16 +0300

    Test example

---
+++ test/test_example.py
@@ -0,0 +1,20 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_example(driver):
+    driver.get("http://www.google.com/")
+    driver.find_element_by_name("q").send_keys("webdriver")
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'btnK')))
+    driver.find_element_by_name("btnK").click()
+    WebDriverWait(driver, 10).until(EC.title_is("webdriver - Google Search"))
